9b0ad1742c22d35f38d467e5494fd3075f449b74
Jack (Pixegami) <pixegami@gmail.com>
2025-05-06 23:15:48 +1000

    feat: replaced basic indexer with docling indexer
    Replaced the indexer (which uses basic text chunking) with a smarter, token based indexer (docling). The results are much better.

--- src/impl/indexer.py
+++ src/impl/indexer.py
@@ -1,43 +1,33 @@
+import os
 from typing import List
 from interface.base_datastore import DataItem
 from interface.base_indexer import BaseIndexer
-import PyPDF2
+from docling.document_converter import DocumentConverter
+from docling.chunking import HybridChunker, DocChunk
 
 
 class Indexer(BaseIndexer):
+    def __init__(self):
+        self.converter = DocumentConverter()
+        self.chunker = HybridChunker()
+        # Disable tokenizers parallelism to avoid OOM errors.
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
     def index(self, document_paths: List[str]) -> List[DataItem]:
         items = []
         for document_path in document_paths:
-            new_items = self._simple_pdf_chunker(document_path)
-            items.extend(new_items)
-
-        print(
-            f"✅ Indexer: Created {len(items)} data items from {len(document_paths)} documents."
-        )
+            document = self.converter.convert(document_path).document
+            chunks: List[DocChunk] = self.chunker.chunk(document)
+            items.extend(self._items_from_chunks(chunks))
         return items
 
-    def _simple_pdf_chunker(
-        self, pdf_path: str, chunk_size: int = 200
-    ) -> List[DataItem]:
-
+    def _items_from_chunks(self, chunks: List[DocChunk]) -> List[DataItem]:
         items = []
-        with open(pdf_path, "rb") as pdf_file:
-            pdf_reader = PyPDF2.PdfReader(pdf_file)
-            doc_index = 0
-
-            for page_num in range(len(pdf_reader.pages)):
-                page = pdf_reader.pages[page_num]
-                page_text = page.extract_text().strip().replace("\n", "")
-                start_index = 0
-
-                while start_index < len(page_text):
-                    end_index = min(start_index + chunk_size, len(page_text))
-                    chunk = page_text[start_index:end_index]
-                    source = f"{pdf_path}:{doc_index}"
-                    item = DataItem(content=chunk, source=source)
-                    items.append(item)
-                    start_index = end_index
-                    doc_index += 1
+        for i, chunk in enumerate(chunks):
+            content_headings = "## " + ", ".join(chunk.meta.headings)
+            content_text = f"{content_headings}\n{chunk.text}"
+            source = f"{chunk.meta.origin.filename}:{i}"
+            item = DataItem(content=content_text, source=source)
+            items.append(item)
 
         return items
